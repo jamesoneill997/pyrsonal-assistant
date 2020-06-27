b47c7329e1e9731669293ab4b97994f81fd948cb
jamesoneill997 <jamesoneill997@gmail.com>
2020-06-27 11:54:05 +0100

    switched from Sphynx to Google as results were better

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import pyaudio
 import speech_recognition as sr
+import os
 
 
 def main():
     r = sr.Recognizer()
+    commands = ["launch", "open", "activate"]
 
     with sr.Microphone() as source:
         print("Say something!")
         audio = r.listen(source)
 
     try:
-        print(r.recognize_sphinx(audio))
+        text = r.recognize_google(audio).split()
+        command = text[0]
+        if not command in commands:
+            print(text[0] + " is not a command")
+
+        if command == "launch":
+            application = [text[i]+ " " for i in range(1,len(text))]
+            print(application)
+            if application[0].lower().strip() in ["google", "chrome"]:
+                print("Launching Chrome...")
+                os.system("google-chrome")
+
+
+
+
     #bad audio
     except sr.UnknownValueError:
         print("Sphinx could not understand audio")
