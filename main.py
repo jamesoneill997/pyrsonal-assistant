7cda0e6503012c081af63425bfedd79e412b8060
jamesoneill997 <jamesoneill997@gmail.com>
2020-06-27 14:35:27 +0100

    added greetings

--- main.py
+++ main.py
@@ -1,38 +1,47 @@
 import pyaudio
 import speech_recognition as sr
 import os
+from playsound import playsound as ps
 
 
 def main():
     r = sr.Recognizer()
-    commands = ["launch", "open", "activate"]
+    greetings = ["test", "hello", "hi"]
+    commands = ["launch", "open", "activate", "search"]
 
     with sr.Microphone() as source:
+        ps("audio.mp3")
         print("Say something!")
-        audio = r.listen(source)
+        audio = r.listen(source, timeout=5)
 
     try:
         text = r.recognize_google(audio).split()
-        command = text[0]
-        if not command in commands:
+        command = text[0].strip()
+        if not command in commands and command not in greetings:
             print(text[0] + " is not a command")
 
         if command == "launch":
-            application = [text[i]+ " " for i in range(1,len(text))]
+            application = text[1]
             print(application)
             if application[0].lower().strip() in ["google", "chrome"]:
-                print("Launching Chrome...")
-                os.system("google-chrome")
+                chrome_launch()
+
+        if command in greetings:
+            print("Hey there, my name is Pylot, how can I help?")    
 
 
 
 
     #bad audio
     except sr.UnknownValueError:
-        print("Sphinx could not understand audio")
+        print("Pyrsonal Assistant could not understand audio")
     #req error
     except sr.RequestError as e:
-        print("Sphinx error; {0}".format(e))
+        print("Error; {0}".format(e))
+
+    def chrome_launch():
+        print("Launching Chrome...")
+        os.system("google-chrome")
 
 if __name__ == "__main__":
     main()
