8481bf5e22c682a66fa06c3c0c240cd4c1909146
jamesoneill997 <jamesoneill997@gmail.com>
2020-06-27 10:14:03 +0100

    dep issues resolved

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import pyaudio
 import speech_recognition as sr
 
+
 def main():
-    print("Imports successful")
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
+
+    try:
+        print(r.recognize_sphinx(audio))
+    except sr.UnknownValueError:
+        print("Sphinx could not understand audio")
+    except sr.RequestError as e:
+        print("Sphinx error; {0}".format(e))
 
 if __name__ == "__main__":
     main()
